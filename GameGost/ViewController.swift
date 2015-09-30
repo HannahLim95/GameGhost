6ab149c04c320259087d90e139b14b6dbccd2447
Hannah Lim <hannahlim@MacBook-Pro-van-Hannah.local>
2015-09-30 22:00:34 +0200

    Initial Commit

---
+++ GameGost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameGost
+//
+//  Created by Hannah Lim on 30-09-15.
+//  Copyright (c) 2015 Hannah Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
